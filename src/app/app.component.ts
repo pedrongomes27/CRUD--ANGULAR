d8f3e605f94cb880b09d3c8f6b43bbfc50dc868f
mtheus <matheusrobertjs@gmail.com>
2023-03-24 22:00:55 -0300

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css', './app.componentTask.css']
+})
+
+export class AppComponent {
+  title = 'TooDu List';
+}
